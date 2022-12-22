6f4ed78a5994b5df64ed6ddd4aba751b08b25da0
ninolis <97913013+ninolis@users.noreply.github.com>
2022-12-22 12:16:08 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
+'use strict';
 
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+
+let timerId;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function disableStartBtn() {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+}
+
+function disableStopBtn() {
+  refs.stopBtn.disabled = true;
+  refs.startBtn.disabled = false;
+}
+
+function startBodyColor() {
+  if (!timerId) {
+    timerId = setInterval(changeBodyColor, 1000);
+  }
+  disableStartBtn();
+}
+
+function stopBodyColor() {
+  clearInterval(timerId);
+  timerId = null;
+  disableStopBtn();
+}
+
+refs.startBtn.addEventListener('click', startBodyColor);
+refs.stopBtn.addEventListener('click', stopBodyColor);
